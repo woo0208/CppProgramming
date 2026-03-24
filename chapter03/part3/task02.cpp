2085e906365b3ee46a9d3f9af62279dc7a8630e9
woo <jwooh0208@gmail.com>
2026-03-24 12:12:57 +0900

    Update task02.cpp

--- chapter03/part3/task02.cpp
+++ chapter03/part3/task02.cpp
@@ -1,52 +1,46 @@
+// *******************************************
+// 제목: 실습 과제2
+// 날짜: 26년 3월 24일
+// 작성자: 2603043 한정우
+// *******************************************
+
+
 #include <iostream>
 
 using namespace std;
 
-class Triangle {
+class Triangle {											// 클래스 선언
 public:
 	double width;
 	double height;
 	Triangle();
 	Triangle(double a, double b);
 	double getArea();
-	void printTra();
-	void printDead();
+	~Triangle();
 };
 
-Triangle::Triangle() : Triangle(1, 1) {
+Triangle::Triangle() : Triangle(1, 1) {						// 생성자1 선언 (바디가 아닌 초기화 리스트 사용)
 };
 
-Triangle::Triangle(double a, double b) : width(a), height(b){
+Triangle::Triangle(double a, double b) : width(a), height(b) {     // 생성자2 선언 (바디가 아닌 초기화 리스트 사용 / 바로 출력 생성)
 	cout << "밑변 " << width << "높이 " << height << "인 삼각형 생성" << endl;
 }
 
-double Triangle::getArea() {
+Triangle::~Triangle() {										// 소멸자 선언 (중복불가니까 1개만 / 바로 출력 생성) 
+	cout << "밑변 " << width << "높이 " << height << "인 삼각형 소멸" << endl;
+};
+
+
+double Triangle::getArea() {								// 면적 계산 함수
 	return width * height / 2;
 }
-
-void Triangle::printTra() {
-	cout << "삼각형의 면적은 " << getArea() << endl;
-}
-
-
-void Triangle::printDead() {
-	cout << "밑변 " << width << "높이 " << height << "인 삼각형 소멸" << endl;
-}
-
-
 
 
 
 int main() {
-	Triangle tr1;
-	tr1.printTra();
-
-	Triangle tr2(2, 4);
-	tr2.printTra();
-
-	tr2.printDead();
-	tr1.printDead();
-
-
+	Triangle tri1;
+	cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+	Triangle tri2(2, 4);
+	cout << "삼각형의 면적은 " << tri2.getArea() << endl;
 	return 0;
 }
