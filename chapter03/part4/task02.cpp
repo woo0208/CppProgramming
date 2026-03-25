71bd8f92cf05db6ff3d879be029d9c8d53fd71bf
woo <jwooh0208@gmail.com>
2026-03-25 10:36:38 +0900

    Update task02.cpp

--- chapter03/part4/task02.cpp
+++ chapter03/part4/task02.cpp
@@ -2,20 +2,26 @@
 
 using namespace std;
 
+// 클래스 선언
 class Circle {
+	// 기본 접근 지정자 private
 	int radius;
+	// 외부 접근을 위해 public 사용
 public:
 	Circle();
 	void setRadius(int r);
 	int getRadius();
 };
 
+// 생성자 선언 (기본값 1로 초기화)
 Circle::Circle() {
 	radius = 1;
 }
 
 
+// 외부에서 값에 접근하여 수정할 수 있도록 set 함수 추가
 void Circle::setRadius(int r) {
+	// 매개변수가 음수일 경우 예외로직 추가
 	if (r < 0) {
 		cout << "음수는 입력할 수 없습니다." << endl;
 		radius = 1;
@@ -26,6 +32,7 @@ void Circle::setRadius(int r) {
 	}
 }
 
+// 외부에서 값을 읽어올 수 있도록 get 함수 추가
 int Circle::getRadius() {
 	return radius;
 }
