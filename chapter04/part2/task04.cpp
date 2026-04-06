337a0e94f27332b8155de6c49e8dbea43c4f3161
woo <jwooh0208@gmail.com>
2026-04-06 10:53:27 +0900

    Create task04.cpp

---
+++ chapter04/part2/task04.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 0;
+	cout << "입력할 정수의 개수를 입력하세요: ";
+	cin >> n;
+	cout << n << "개의 실수를 입력하시오.\n";
+	double* num = new double[n];
+	double max = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+		if (num[i] > max) {
+			max = num[i];
+		}
+	}
+
+	cout << "최대값은 " << max << "입니다" << endl;
+
+	return 0;
+}
