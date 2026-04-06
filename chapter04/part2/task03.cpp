e373fd0b6941fa3e49fd9df385812712dc4dfd14
woo <jwooh0208@gmail.com>
2026-04-06 10:38:09 +0900

    Create task03.cpp

---
+++ chapter04/part2/task03.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 0;
+	cout << "입력할 정수의 개수를 입력하세요: ";
+	cin >> n;
+	cout << n << "개의 정수를 입력하시오.\n";
+	int* num = new int[n];
+	int total = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+		total += num[i];
+	}
+
+	int avg = total / n;
+
+	cout << "평균값은 " << avg << "입니다" << endl;
+
+	return 0;
+}
