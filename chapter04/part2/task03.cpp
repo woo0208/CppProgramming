3332030b22eece7ecc345314ae605629273cd703
woo <jwooh0208@gmail.com>
2026-04-06 10:55:25 +0900

    Update task03.cpp

--- chapter04/part2/task03.cpp
+++ chapter04/part2/task03.cpp
@@ -9,6 +9,10 @@ int main() {
 	cin >> n;
 	cout << n << "개의 정수를 입력하시오.\n";
 	int* num = new int[n];
+	if (!num) {
+	cout << "메모리를 할당할 수 없습니다.";
+	return -1;
+	}
 	int total = 0;
 	for (int i = 0; i < n; i++) {
 		cin >> num[i];
