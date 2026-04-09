7ac4958a903bd07af9ce778298b0119c92c42aa6
woo <jwooh0208@gmail.com>
2026-04-09 14:17:11 +0900

    Update task03.cpp

--- chapter04/part3/task03.cpp
+++ chapter04/part3/task03.cpp
@@ -1,32 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Circle{
-int radius;
+
+class Circle {
+    int radius;
 public:
-Circle();
-Circle(int r);
-~Circle();
-void setRadius(int r) { radius = r; }
-double getArea() { return 3.14 * radius * radius; }
+    Circle() { radius = 1; }
+    Circle(int r) { radius = r; }
+    double getArea() { return 3.14 * radius * radius; }
 };
-Circle::Circle() {
-	radius = 1; cout << "생성자실행 radius = " << radius << endl;
-}
-Circle::Circle(int r) {
-	radius = r; cout << "생성자실행 radius = " << radius << endl;
-}
-Circle::~Circle() {
-	cout << "소멸자실행radius = " << radius << endl;
-}
 
 int main() {
-	Circle* pArray = new Circle[3];
-	for (int i = 0; i < 3; i++) {
-		if (i == 2) {
-		cout << pArray[i].getArea() << "\n";
-		}
-	}
+    Circle* pArray = new Circle[3];
 
-	delete[] pArray;
-	return 0;
+    for (int i = 0; i < 3; i++) {
+        cout << pArray[i].getArea() << '\n';
+    }
+
+    delete[] pArray;
+
+    return 0;
 }
+
+
+// 배열 인덱스([]) 사용(권장) : 포인터 변수 자체를 이동시키는 대신, 인덱스를 사용하여 각 요소에 접근합니다.
+
+//  임시 포인터 사용 : 포인터 연산이 반드시 필요하다면, 원본 주소를 보관할 별도의 포인터를 할당하여 사용합니다.
