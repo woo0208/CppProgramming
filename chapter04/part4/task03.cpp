be14216e518543bd92d8a178d28d6e2cfff7181f
woo <jwooh0208@gmail.com>
2026-04-16 17:39:28 +0900

    Create task03.cpp

---
+++ chapter04/part4/task03.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+	string st;
+	cout << "문자열 입력: ";
+	getline(cin, st);
+	int count = 0;
+	for (int i = 0; i < st.length(); i++) {
+		if (st[i] == 'a') {
+			count++;
+		}
+	}
+
+	cout << "문자 a는 " << count << "개 있습니다." << endl;
+	return 0;
+}
