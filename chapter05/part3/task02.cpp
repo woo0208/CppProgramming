6b971496bbbb8a2d11514f663fe8e584de499633
woo <jwooh0208@gmail.com>
2026-05-11 10:14:51 +0900

    Create task02.cpp

---
+++ chapter05/part3/task02.cpp
@@ -0,0 +1,25 @@
+// *******************************************
+// 제목: 실습 과제2
+// 날짜: 26년 5월 11일
+// 작성자: 2603043 한정우
+// *******************************************
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void change(string &s) {
+	for (int i = 0; i < s.length(); i++) {
+		s[i] = toupper(s[i]);
+		cout << "변환" << s << endl;
+	}
+}
+
+int main() {
+	string s1("hello");
+	cout << "변환전 문자열" << s1 << endl;
+	change(s1);
+	cout << "변환후 문자열" << s1 << endl;
+	return 0;
+}
