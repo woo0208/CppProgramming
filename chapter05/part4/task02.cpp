452e248b4c8b8d970a629b4e2fdf141516876e68
woo <jwooh0208@gmail.com>
2026-05-14 21:43:29 +0900

    Update task02.cpp

--- chapter05/part4/task02.cpp
+++ chapter05/part4/task02.cpp
@@ -4,78 +4,45 @@
 //  작성자  :  2603043 한정우
 // **********************************************
 
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstring>
-
+#include <string> // string 클래스 사용을 위해 추가
 using namespace std;
 
-class Person {
-    char* name;
+class Person { // Person 클래스 선언
+    string name; // char* name에서 string으로 변경
     int id;
-
 public:
-    Person(int id, const char* name);            // 생성자
-    Person(const Person& person);               // 복사 생성자
-    ~Person();                                  // 소멸자
-
-    void changeName(const char* name);
-
-    void show() {
-        cout << id << ',' << name << endl;
-    }
+    Person(int id, string name); // 생성자 매개변수 변경
+    void changeName(string name);
+    void show() { cout << id << ',' << name << endl; }
 };
 
-// 생성자
-Person::Person(int id, const char* name) {
+// 생성자 구현
+Person::Person(int id, string name) { 
     this->id = id;
-
-    int len = strlen(name);
-    this->name = new char[len + 1];   // 메모리 할당
-    strcpy(this->name, name);
+    this->name = name; // 동적 할당이나 strcpy 없이 단순 대입으로 처리
 }
 
-// 복사 생성자 (깊은 복사)
-Person::Person(const Person& person) {
-    this->id = person.id;
-
-    int len = strlen(person.name);
-    this->name = new char[len + 1];   // 새 메모리 할당
-    strcpy(this->name, person.name);
-
-    cout << "복사 생성자 실행. 원본 객체의 이름 "
-        << this->name << endl;
-}
-
-// 소멸자
-Person::~Person() {
-    if (name)
-        delete[] name;
-}
-
-// 이름 변경
-void Person::changeName(const char* name) {
-    if (strlen(name) > strlen(this->name))
-        return;
-
-    strcpy(this->name, name);
+// 이름 변경 함수 구현
+void Person::changeName(string name) {
+    this->name = name;
 }
 
 int main() {
-    Person father(1, "Kitae");      // 생성자 호출
-    Person daughter(father);       // 복사 생성자 호출
-
+    Person father(1, "Kitae"); // father 객체 생성자 호출
+    
+    // 컴파일러가 자동 생성한 디폴트 복사 생성자가 호출됨.
+    // string 멤버 변수의 복사 대입이 이루어지며, string 내부적으로 깊은 복사가 안전하게 수행됨.
+    Person daughter(father); 
+    
     cout << "daughter 객체 생성 직후 ----" << endl;
-
-    father.show();
-    daughter.show();
-
-    daughter.changeName("Grace");
-
+    father.show(); // father 객체 출력
+    daughter.show(); // daughter 객체 출력
+    
+    daughter.changeName("Grace"); // daughter의 이름을 "Grace"로 변경
     cout << "daughter 이름을 Grace로 변경한 후 ----" << endl;
-
-    father.show();
-    daughter.show();
-
-    return 0;
+    father.show(); // father 객체 출력
+    daughter.show(); // daughter 객체 출력
+    
+    return 0; 
 }
