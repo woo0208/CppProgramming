ba54bd096893834106a187a784d21c74ae10abf9
woo <jwooh0208@gmail.com>
2026-05-18 10:45:42 +0900

    Create task04.cpp

---
+++ chapter06/part1/task04.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Point3D {
+	int arr[3];
+public:
+	Point3D() {
+		arr[0] = 0;
+		arr[1] = 0;
+		arr[2] = 0;
+	}
+	Point3D(int x) {
+		arr[0] = x;
+		arr[1] = 0;
+		arr[2] = 0;
+	}
+	Point3D(int x, int y) {
+		arr[0] = x;
+		arr[1] = y;
+		arr[2] = 0;
+	}
+	Point3D(int x, int y, int z) {
+		arr[0] = x;
+		arr[1] = y;
+		arr[2] = z;
+	}
+	void show() {
+		cout << "3차원 점의 좌표는 (" << arr[0] << "," << arr[1] << "," << arr[2] << ")" << endl;
+	}
+};
+
+int main() {
+	Point3D p0;
+	p0.show();
+	Point3D p1(1);
+	p1.show();
+	Point3D p2(1, 2);
+	p2.show();
+	Point3D p3(1, 2, 3);
+	p3.show();
+	return 0;
+}
