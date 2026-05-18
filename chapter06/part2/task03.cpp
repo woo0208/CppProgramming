057acf543aaa07563e659c7fc5726ab6c12aab4f
woo <jwooh0208@gmail.com>
2026-05-18 10:39:21 +0900

    Create task03.cpp

---
+++ chapter06/part2/task03.cpp
@@ -0,0 +1,24 @@
+class Point3D {
+	int arr[3];
+public:
+	Point3D(int x = 0, int y = 0, int z = 0) {
+		arr[0] = x;
+		arr[1] = y;
+		arr[2] = z;
+	}
+	void show() {
+		cout << "3차원 점의 좌표는 (" << arr[0] << "," << arr[1] << "," << arr[2] << ")" << endl;
+	}
+};
+
+int main() {
+	Point3D p0;
+	p0.show();
+	Point3D p1(1);
+	p1.show();
+	Point3D p2(1, 2);
+	p2.show();
+	Point3D p3(1, 2, 3);
+	p3.show();
+	return 0;
+}
