bb6699dcacbfa4ba00df441ca4ccb499345eed23
woo <jwooh0208@gmail.com>
2026-05-18 10:46:57 +0900

    Create task02.cpp

---
+++ chapter06/part2/task02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+	int width, height;
+public:
+	Rectangle(int x=1, int y=1) {
+		width = x;
+		height = y;
+	}
+	void show() {
+		cout << "사각형 폭은 " << width << " 높이는 " << height << endl;
+	}
+};
+
+int main() {
+	Rectangle rect0;
+	rect0.show();
+	Rectangle rect1(10);
+	rect1.show();
+	Rectangle rect2(10, 20);
+	rect2.show();
+	return 0;
+}
