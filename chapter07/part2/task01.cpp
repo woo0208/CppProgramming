3b08d9b09023666648051765b930c9f22ed4359e
woo <jwooh0208@gmail.com>
2026-06-01 10:14:16 +0900

    Create task01.cpp

---
+++ chapter07/part2/task01.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+class Complex {
+	int x;
+	int y;
+public:
+	Complex operator+(int c);
+	Complex operator+(Complex c);
+	Complex();
+	Complex(int x, int y);
+	void show();
+};
+
+Complex::Complex() : x(0), y(0) {}
+Complex::Complex(int a,int b) : x(a), y(b) {}
+
+Complex Complex::operator+(int x) {
+	Complex cm;
+	cm.x = this->x + x;
+	cm.y = this->y + x;
+	return cm;
+}
+
+Complex Complex::operator+(Complex c) {
+	Complex cm;
+	cm.x = this->x + c.x;
+	cm.y = this->y + c.y;
+	return cm;
+}
+
+void Complex::show() {
+	cout << x << "+" << y << "j" << endl;
+}
+
+int main() {
+	Complex x(2, 3),y(-5,10),sum;
+	sum = x + y;
+	cout << "두 복소수의 합은";
+	sum.show();
+}
+
+
+
+// 사칙연산 연산자 만들기 => 시험 출제 예상
